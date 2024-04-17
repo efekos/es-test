775fae90ca4336de1fffef9372a94d58359cf386
efekos <hefeolgun10@gmail.com>
2024-04-17 17:46:24 +0300

    🧪 add a test test

---
+++ src/test.ts
@@ -0,0 +1,12 @@
+import { describe, it } from './index.js';
+import { expect } from 'chai';
+
+describe('test suite',()=>{
+
+    it('should work',()=>{
+
+        expect('foo').to.equal('bar');
+
+    });
+
+});
